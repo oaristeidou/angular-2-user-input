41ced34e46634bc58583b0a4a2b90df0add157fa
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-05-05 02:34:30 +0200

    - This won't work at all unless you bind to an event.

--- src/app/loop-back/loop-back.component.ts
+++ src/app/loop-back/loop-back.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loop-back',
   templateUrl: './loop-back.component.html',
   styleUrls: ['./loop-back.component.css']
 })
-export class LoopBackComponent implements OnInit {
+export class LoopBackComponent{
+  values:string ='';
 
-  constructor() { }
-
-  ngOnInit() {
+  onKeyUpEvent(value:string){
+    this.values += value + "|";
   }
-
 }
